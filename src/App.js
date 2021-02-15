08db8f0c3d50b1f9be6e116d0ac73f943d9f027d
Omkar2310 <mankar.omkar2310@gmail.com>
2021-02-15 21:00:19 +0530

    added basic chatengine and 2 users

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="5e611b1a-ca8f-4097-b766-81b4847fb2e5"
+      userName="Omi"
+      userSecret="123456"
+      ></ChatEngine>
+  );
+}
+
+export default App;
